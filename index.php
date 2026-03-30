d60c3b45ebda2092dc56608cada57b5465568e61
diyunih <diyunih77@gmail.com>
2026-03-30 22:10:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
         <div class="hero-left">
             <h1>Tumbuhkan bisnismu dengan TikTok</h1>
             <p>Jangkau target audiensmu dan dapatkan hasil dengan cepat menggunakan TikTok Ads Manager. Dengan basis audiens yang besar dan fitur yang mudah digunakan, siapapun bisa jadi ahli pemasaran.</p>
-            <a href="https//:tiktok.vipaffi.com/auth/register" class="btn-mulai">Mulai</a>
+            <a href="https://tiktokaffi.vipaffi.com/auth/register" class="btn-mulai">Mulai</a>
         </div>
         <div class="hero-right">
             <div class="image-container">
